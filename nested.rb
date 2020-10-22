004203a3de5169c30a71aa755a12bdeb02c5bccc
Jason  Zhou <jzhou35@buffalo.edu>
2020-10-22 05:04:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,22 +21,6 @@ end
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	programmer_hash[:alan_kay][:known_for]
-	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-
 end
 
 def dennis_ritchies_language
@@ -65,23 +49,8 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-programmer_hash[:yukihiro_matsumoto => {:known_for => "Ruby",:languages => ["LISP","C"]}]
-	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-
+  programmer_hash[:yukihiro_matsumoto => {:known_for => "Ruby",:languages => ["LISP","C"]}]
+  programmer_hash
 end
 
 def changing_alan
@@ -89,22 +58,6 @@ def changing_alan
   # return the entire updated hash
   programmer_hash[:alan_kay][:known_for] = alans_new_info
   programmer_hash
-	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-
 end
 
 def adding_to_dennis
@@ -112,20 +65,4 @@ def adding_to_dennis
   # return the entire updated hash
   programmer_hash[:dennis_ritchie][:languages].push("Assembly")
   programmer_hash
-	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-
 end
